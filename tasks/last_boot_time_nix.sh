7030769ff2e1991c55a930484bcc3afd63813a5b
Nick Burgan-Illig <nick.burgan-illig@puppet.com>
2021-08-12 00:23:30 +0000

    (PE-31824) Modify result of 'last' to remove current time
    When doing 'last -1 reboot' on RedHat 6/7-based platforms as well as SLES 12, the end of this line will show the current time, since it shows both the start and end time of the session. Since the plan is simply comparing the raw string to see if we've rebooted, this makes it think we've always rebooted, since the end of this string changes every second. This commit slices off the end time, leaving only the login time.

--- tasks/last_boot_time_nix.sh
+++ tasks/last_boot_time_nix.sh
@@ -3,4 +3,4 @@
 # Lifted from puppetlabs-reboot
 
 # Try -F for time in seconds, fall back to without if unavailable
-last -1 -F reboot 2>/dev/null || last -1 reboot
+(last -1 -F reboot 2>/dev/null || last -1 reboot) | sed 's/\s-\s.*$//'
